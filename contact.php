52dabfc8e22cd899e7dc1692224bfe7d148290b3
samarth-2 <samarth.2032@gmail.com>
2023-08-03 12:41:10 +0530

    final

---
+++ contact.php
@@ -0,0 +1,63 @@
+<?php
+   include 'config.php';
+   session_start();
+
+   $user_id = $_SESSION['user_id'];
+   if(!isset($user_id)){
+      header('location:login.php');
+   }
+
+   if(isset($_POST['send'])){          
+      $name = mysqli_real_escape_string($conn, $_POST['name']);     
+      $email = mysqli_real_escape_string($conn, $_POST['email']);
+      $number = $_POST['number'];
+      $msg = mysqli_real_escape_string($conn, $_POST['message']);
+
+     
+      $select_message = mysqli_query($conn, "SELECT * FROM `message` WHERE name = '$name' AND email = '$email' AND number = '$number' AND message = '$msg'") or die('query failed');
+
+      if(mysqli_num_rows($select_message) > 0){    
+         $message[] = 'Message already sent!';
+      }else{
+        
+         mysqli_query($conn, "INSERT INTO `message`(user_id, name, email, number, message) VALUES('$user_id', '$name', '$email', '$number', '$msg')") or die('query failed');
+         $message[] = 'Message sent successfully!';
+      }
+   }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Contact</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+      <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">       
+   </head>
+
+   <body>
+      <?php include 'header.php'; ?>
+
+      <div class="heading">
+         <h3>contact us</h3>
+      </div>
+
+      <section class="contact">
+         <form action="" method="post">
+            <h3>say something !</h3>
+            <input type="text" name="name" required placeholder="Name" class="box">
+            <input type="email" name="email" required placeholder="Email" class="box">
+            <input type="number" name="number" required placeholder="Phone Number" class="box">
+            <textarea name="message" class="box" placeholder="Message" id="" cols="30" rows="10"></textarea>
+            <input type="submit" value="send message" name="send" class="btn">
+         </form>
+      </section>
+
+      <?php include 'footer.php'; ?>
+
+      <script src="js/script.js"></script>
+   </body>
+</html>
---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+   <div class="box-container">
+      
+      <div class="box">
+         <h3>BookStore</h3>
+         <a href="about.php">About us</a>
+         <a href="contact.php">Contact</a>
+         <a href="#">Privacy Policy</a>
+         <a href="#">Terms & Conditions</a>
+      </div>
+
+      
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+    
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> 210-222-1111 </p>
+         <p> <i class="fas fa-phone"></i> 210-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> katisnikosacc@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Athens, Greece - 56430 </p>
+      </div>
+
+     
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> Linkedin </a>
+      </div>
+   </div>
+
+   <p class="credit"> &copy; copyright   <?php echo date('Y'); ?> by <span>Nick</span> </p>
+</section>
